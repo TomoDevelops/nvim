d79524521f9620c8dfca9a58ebbdfca365fd98ba
TomoDevelops <wsl@tomodevelops.com>
2025-03-07 17:51:49 +0900

    remap

--- lua/tomodevelops/plugins/git.lua
+++ lua/tomodevelops/plugins/git.lua
@@ -14,7 +14,7 @@ return {
         "lewis6991/gitsigns.nvim",
         config = function()
             require("gitsigns").setup()
-            vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+            -- vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
         end,
     }
 }
